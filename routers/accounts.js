7274a8fd9f6993d5d21346bc6d64d978c511cec2
Fabio Bertuzzi <fabio@bertuzzi.de>
2016-12-11 18:30:34 +0100

    Adding Router to account.js

--- routers/accounts.js
+++ routers/accounts.js
@@ -1,3 +1,11 @@
 /**
  * Created by fabio on 11.12.2016.
  */
+let express = require('express');
+let router = express.Router();
+let path = require('path');
+let Hashmap = require('hashmap');
+let authenticatedUsers = new Hashmap();
+let users = require('./api.js').users;
+
+module.exports = router;
