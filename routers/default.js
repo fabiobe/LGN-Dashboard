ca5a963c7529bb71c4bd1e6e144856fcaaee7017
Fabio Bertuzzi <fabio@bertuzzi.de>
2016-12-17 15:24:34 +0100

    Adding login to every page!

--- routers/default.js
+++ routers/default.js
@@ -4,8 +4,6 @@
 let express = require('express');
 let router = express.Router();
 let path = require('path');
-let Hashmap = require('hashmap');
-let authenticatedUsers = new Hashmap();
 let users = require('./api.js').users;
 
 router.get('/', (req, res) => {
@@ -19,44 +17,108 @@ router.get('/', (req, res) => {
 
 });
 
+router.get('/profile', (req, res) => {
+    if (req.cookies.token != undefined) {
+        if (users.indexOf(req.cookies.token) > -1) {
+            res.sendFile(path.join(__dirname, '../views/default/profile.html'));
+            return;
+        }
+    }
+    res.sendFile(path.join(__dirname, '../views/default/login.html'));
+});
+
 router.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/default/login.html'));
 });
 
 router.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/default/dashboard.html'));
+    if (req.cookies.token != undefined) {
+        if (users.indexOf(req.cookies.token) > -1) {
+            res.sendFile(path.join(__dirname, '../views/default/dashboard.html'));
+            return;
+        }
+    }
+    res.sendFile(path.join(__dirname, '../views/default/login.html'));
 });
 
 router.get('/reports', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/default/reports.html'));
+    if (req.cookies.token != undefined) {
+        if (users.indexOf(req.cookies.token) > -1) {
+            res.sendFile(path.join(__dirname, '../views/default/reports.html'));
+            return;
+        }
+    }
+    res.sendFile(path.join(__dirname, '../views/default/login.html'));
 });
 
 router.get('/users', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/default/users.html'));
+    if (req.cookies.token != undefined) {
+        if (users.indexOf(req.cookies.token) > -1) {
+            res.sendFile(path.join(__dirname, '../views/default/users.html'));
+            return;
+        }
+    }
+    res.sendFile(path.join(__dirname, '../views/default/login.html'));
 });
 
 router.get('/wifi', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/default/wifi.html'));
+    if (req.cookies.token != undefined) {
+        if (users.indexOf(req.cookies.token) > -1) {
+            res.sendFile(path.join(__dirname, '../views/default/wifi.html'));
+            return;
+        }
+    }
+    res.sendFile(path.join(__dirname, '../views/default/login.html'));
 });
 
 router.get('/email', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/default/email.html'));
+    if (req.cookies.token != undefined) {
+        if (users.indexOf(req.cookies.token) > -1) {
+            res.sendFile(path.join(__dirname, '../views/default/email.html'));
+            return;
+        }
+    }
+    res.sendFile(path.join(__dirname, '../views/default/login.html'));
 });
 
 router.get('/tasks', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/default/tasks.html'));
+    if (req.cookies.token != undefined) {
+        if (users.indexOf(req.cookies.token) > -1) {
+            res.sendFile(path.join(__dirname, '../views/default/tasks.html'));
+            return;
+        }
+    }
+    res.sendFile(path.join(__dirname, '../views/default/login.html'));
 });
 
 router.get('/servers', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/default/servers.html'));
+    if (req.cookies.token != undefined) {
+        if (users.indexOf(req.cookies.token) > -1) {
+            res.sendFile(path.join(__dirname, '../views/default/servers.html'));
+            return;
+        }
+    }
+    res.sendFile(path.join(__dirname, '../views/default/login.html'));
 });
 
 router.get('/printers', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/default/printers.html'));
+    if (req.cookies.token != undefined) {
+        if (users.indexOf(req.cookies.token) > -1) {
+            res.sendFile(path.join(__dirname, '../views/default/printers.html'));
+            return;
+        }
+    }
+    res.sendFile(path.join(__dirname, '../views/default/login.html'));
 });
 
 router.get('/settings', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/default/settings.html'));
+    if (req.cookies.token != undefined) {
+        if (users.indexOf(req.cookies.token) > -1) {
+            res.sendFile(path.join(__dirname, '../views/default/settings.html'));
+            return;
+        }
+    }
+    res.sendFile(path.join(__dirname, '../views/default/login.html'));
 });
 
 module.exports = router;
