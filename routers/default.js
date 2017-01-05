b014f211400cd16fe5f5995419ba7a0282f1e572
Fabio Bertuzzi <fabio@bertuzzi.de>
2017-01-05 17:15:44 +0100

    Adding maintenance

--- routers/default.js
+++ routers/default.js
@@ -4,6 +4,7 @@
 let express = require('express');
 let router = express.Router();
 let path = require('path');
+let mysql = require("mysql");
 let users = require('./api.js').users;
 let mysql_config = require('./../config/mysql.json');
 
