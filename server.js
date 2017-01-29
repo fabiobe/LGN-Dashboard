46a9226a4af732f301ba1e7833531dcfe6a7ebc6
Fabio Bertuzzi <fabio@bertuzzi.de>
2017-01-29 13:14:36 +0100

    Changing API

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ let config = require('./config/config.json');
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser);
 app.use(express.static(path.join(__dirname, 'public/')));
 
 if (config.setup) {
